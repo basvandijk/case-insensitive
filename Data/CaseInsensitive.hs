b66fd1c445ad34cb4192eb57c3a1b6b52fd6f6eb
Bas van Dijk <v.dijk.bas@gmail.com>
2013-01-14 19:57:31 +0100

    Dropped some language extensions - UnicodeSyntax, TypeSynonymInstances and FlexibleInstances are dropped - A hidden foldCaseList method is added to the FoldCase class   which is used to case fold Strings. - A FoldCase instance for Char is added. - Bump version from 0.4.0.4 to 1.0.

--- Data/CaseInsensitive.hs
+++ Data/CaseInsensitive.hs
@@ -1,10 +1,4 @@
-{-# LANGUAGE CPP
-           , NoImplicitPrelude
-           , UnicodeSyntax
-           , TypeSynonymInstances
-           , DeriveDataTypeable
-           , FlexibleInstances
-  #-}
+{-# LANGUAGE CPP, NoImplicitPrelude, DeriveDataTypeable #-}
 
 #if __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
@@ -13,7 +7,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.CaseInsensitive
--- Copyright   :  (c) 2011-2012 Bas van Dijk
+-- Copyright   :  (c) 2011-2013 Bas van Dijk
 -- License     :  BSD-style (see the file LICENSE)
 -- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
 --
@@ -23,6 +17,9 @@
 -- import           Data.CaseInsensitive  ( CI )
 -- import qualified Data.CaseInsensitive as CI
 -- @
+--
+-- /Note that the FoldCase instance for ByteStrings is only/
+-- /guaranteed to be correct for ASCII encoded strings!/
 --
 -----------------------------------------------------------------------------
 
@@ -40,16 +37,19 @@ module Data.CaseInsensitive ( CI
 
 -- from base:
 import Data.Bool     ( (||) )
+import Data.Char     ( Char, toLower )
 import Data.Eq       ( Eq, (==) )
-import Data.Ord      ( Ord, compare )
 import Data.Function ( on )
 import Data.Monoid   ( Monoid, mempty, mappend )
+import Data.Ord      ( Ord, compare )
 import Data.String   ( IsString, fromString )
 import Data.Typeable ( Typeable )
 import Data.Word     ( Word8 )
 import Prelude       ( String, (.), fmap, (&&), (+), (<=), (>=), otherwise )
 import Text.Read     ( Read, readPrec )
-import Text.Show     ( Show, showsPrec, ShowS )
+import Text.Show     ( Show, showsPrec )
+
+import qualified Data.List as L ( map )
 
 -- from bytestring:
 import qualified Data.ByteString             as B    ( ByteString, map )
@@ -60,14 +60,14 @@ import qualified Data.Text      as T  ( Text, toCaseFold )
 import qualified Data.Text.Lazy as TL ( Text, toCaseFold, pack, unpack )
 
 -- from hashable:
-import Data.Hashable ( Hashable, hashWithSalt )
+import           Data.Hashable ( Hashable, hashWithSalt )
 
 --------------------------------------------------------------------------------
 -- Case Insensitive Strings
 --------------------------------------------------------------------------------
 
 {-| A @CI s@ provides /C/ase /I/nsensitive comparison for the string-like type
-@s@ (for example: 'String', 'T.Text', 'B.ByteString', 'ShowS', etc.).
+@s@ (for example: 'String', 'T.Text', 'B.ByteString', etc.).
 
 Note that @CI s@ has an instance for 'IsString' which together with the
 @OverloadedStrings@ language extension allows you to write case insensitive
@@ -79,37 +79,37 @@ True
 @
 
 -}
-data CI s = CI { original   ∷ !s -- ^ Retrieve the original string-like value.
-               , foldedCase ∷ !s -- ^ Retrieve the case folded string-like value.
-                                 --   (Also see 'foldCase').
+data CI s = CI { original   :: !s -- ^ Retrieve the original string-like value.
+               , foldedCase :: !s -- ^ Retrieve the case folded string-like value.
+                                  --   (Also see 'foldCase').
                }
           deriving Typeable
 
 -- | Make the given string-like value case insensitive.
-mk ∷ FoldCase s ⇒ s → CI s
+mk :: FoldCase s => s -> CI s
 mk s = CI s (foldCase s)
 
 -- | Transform the original string-like value but keep it case insensitive.
-map ∷ FoldCase s2 ⇒ (s1 → s2) → (CI s1 → CI s2)
+map :: FoldCase s2 => (s1 -> s2) -> (CI s1 -> CI s2)
 map f = mk . f . original
 
-instance (IsString s, FoldCase s) ⇒ IsString (CI s) where
+instance (IsString s, FoldCase s) => IsString (CI s) where
     fromString = mk . fromString
 
-instance Monoid s ⇒ Monoid (CI s) where
+instance Monoid s => Monoid (CI s) where
     mempty = CI mempty mempty
     CI o1 l1 `mappend` CI o2 l2 = CI (o1 `mappend` o2) (l1 `mappend` l2)
 
-instance Eq s ⇒ Eq (CI s) where
+instance Eq s => Eq (CI s) where
     (==) = (==) `on` foldedCase
 
-instance Ord s ⇒ Ord (CI s) where
+instance Ord s => Ord (CI s) where
     compare = compare `on` foldedCase
 
-instance (Read s, FoldCase s) ⇒ Read (CI s) where
+instance (Read s, FoldCase s) => Read (CI s) where
     readPrec = fmap mk readPrec
 
-instance Show s ⇒ Show (CI s) where
+instance Show s => Show (CI s) where
     showsPrec prec = showsPrec prec . original
 
 instance Hashable s => Hashable (CI s) where
@@ -127,22 +127,31 @@ instance Hashable s => Hashable (CI s) where
 -- Programs that require locale sensitivity should use appropriate versions of
 -- the case mapping functions from the @text-icu@ package:
 -- <http://hackage.haskell.org/package/text-icu>
-class FoldCase s where foldCase ∷ s → s
+class FoldCase s where
+    foldCase :: s -> s
 
--- | Note that @foldCase = 'B.map' toLower@ which is only guaranteed to be correct for ASCII encoded strings!
-instance FoldCase B.ByteString where foldCase = B.map toLower
+    foldCaseList :: [s] -> [s]
+    foldCaseList = L.map foldCase
 
--- | Note that @foldCase = 'BL.map' toLower@ which is only guaranteed to be correct for ASCII encoded strings!
-instance FoldCase BL.ByteString where foldCase = BL.map toLower
+instance FoldCase a => FoldCase [a] where
+    foldCase = foldCaseList
 
-instance FoldCase String  where foldCase = TL.unpack . TL.toCaseFold . TL.pack
+-- | Note that @foldCase@ on @'B.ByteString's@ is only guaranteed to be correct for ASCII encoded strings!
+instance FoldCase B.ByteString where foldCase = B.map toLower8
+
+-- | Note that @foldCase@ on @'BL.ByteString's@ is only guaranteed to be correct for ASCII encoded strings!
+instance FoldCase BL.ByteString where foldCase = BL.map toLower8
+
+instance FoldCase Char where
+    foldCase     = toLower
+    foldCaseList = TL.unpack . TL.toCaseFold . TL.pack
+
 instance FoldCase T.Text  where foldCase = T.toCaseFold
 instance FoldCase TL.Text where foldCase = TL.toCaseFold
-instance FoldCase ShowS   where foldCase = (foldCase .)
 instance FoldCase (CI s)  where foldCase (CI _ l) = CI l l
 
-toLower :: Word8 -> Word8
-toLower w
+toLower8 :: Word8 -> Word8
+toLower8 w
   |  65 <= w && w <=  90 ||
     192 <= w && w <= 214 ||
     216 <= w && w <= 222 = w + 32
