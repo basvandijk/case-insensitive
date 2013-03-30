1bcbdbfd54e89c576063ee4aec996e138277ea1e
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-30 20:12:48 +0400

    Derive Data class

--- Data/CaseInsensitive.hs
+++ Data/CaseInsensitive.hs
@@ -43,7 +43,7 @@ import Data.Function ( on )
 import Data.Monoid   ( Monoid, mempty, mappend )
 import Data.Ord      ( Ord, compare )
 import Data.String   ( IsString, fromString )
-import Data.Typeable ( Typeable )
+import Data.Data     ( Typeable, Data )
 import Data.Word     ( Word8 )
 import Prelude       ( String, (.), fmap, (&&), (+), (<=), (>=), otherwise )
 import Text.Read     ( Read, readPrec )
@@ -91,7 +91,7 @@ data CI s = CI { original   :: !s -- ^ Retrieve the original string-like value.
                , foldedCase :: !s -- ^ Retrieve the case folded string-like value.
                                   --   (Also see 'foldCase').
                }
-          deriving Typeable
+          deriving (Typeable, Data)
 
 -- | Make the given string-like value case insensitive.
 mk :: FoldCase s => s -> CI s
