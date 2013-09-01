3f922afcc369210afa439bda33c3721764bcddc2
Bas van Dijk <v.dijk.bas@gmail.com>
2013-09-01 11:18:51 +0200

    Mark Data.CaseInsensitive.Internal as Unsafe

--- Data/CaseInsensitive/Internal.hs
+++ Data/CaseInsensitive/Internal.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE CPP, NoImplicitPrelude, DeriveDataTypeable #-}
 
 #if __GLASGOW_HASKELL__ >= 702
-{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE Unsafe #-}
 #endif
 
 -----------------------------------------------------------------------------
