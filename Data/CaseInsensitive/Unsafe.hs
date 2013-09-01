94238b2650614036c4ba7e42aab7813658f1b4c5
Bas van Dijk <v.dijk.bas@gmail.com>
2013-09-01 11:13:37 +0200

    Added the Data.CaseInsensitive.Unsafe module

---
+++ Data/CaseInsensitive/Unsafe.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE CPP, NoImplicitPrelude #-}
+
+#if __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Unsafe #-}
+#endif
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.CaseInsensitive.Unsafe
+-- Copyright   :  (c) 2011-2013 Bas van Dijk
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
+--
+-- Provides an unsafe way to create a case insensitive string-like value.
+--
+-----------------------------------------------------------------------------
+
+module Data.CaseInsensitive.Unsafe   ( unsafeMk ) where
+import Data.CaseInsensitive.Internal ( unsafeMk )
